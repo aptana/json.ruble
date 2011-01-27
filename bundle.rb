973deda5b66521e35018cbd90c4053e2ed130449
Ingo Muschenetz <ingo@appcelerator.com>
2011-01-27 14:36:51 -0800

    Removed non-ASCII characters.

--- bundle.rb
+++ bundle.rb
@@ -2,7 +2,7 @@ require 'ruble'
 
 bundle do |bundle|
   bundle.author = 'Christopher Williams'
-  bundle.copyright = "© Copyright 2010 Aptana Inc. Distributed under the MIT license."
+  bundle.copyright = "Copyright 2010 Aptana Inc. Distributed under the MIT license."
   bundle.repository = "git://github.com/aptana/json.ruble.git"
   bundle.display_name = 'JSON'
   bundle.description =  <<END
