3c4cd223ff4f4bc6505dee7d5e00f3b0d4831740
Allen Yeung <ayeung@aptana.com>
2011-07-21 14:33:01 -0700

    Revert "Fix APSTUD-2134:Add .location property to file templates"
    This reverts commit 54bf23d7e69726f43283866e88e390951d4c94b1.

--- templates/templates.rb
+++ templates/templates.rb
@@ -2,5 +2,9 @@ require 'ruble'
 
 template "JSON Template" do |t|
   t.filetype = "*.json"
-  t.location = "templates/template.json"
+  t.invoke do |context|
+    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.json")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+ end
 end
