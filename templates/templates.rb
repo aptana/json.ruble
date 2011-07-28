5ecb2db73c5e4a7ab52c3819c804e011b944eac4
Ingo Muschenetz <ingo@muschenetz.com>
2011-07-27 20:57:28 -0700

    Corrected APSTUD-3106, where we comment out the JSON template for now, since there was no default content. Studio now presents a \"Blank File\" option instead for that file type.

--- templates/templates.rb
+++ templates/templates.rb
@@ -1,6 +1,7 @@
 require 'ruble'
 
-template "JSON Template" do |t|
-  t.filetype = "*.json"
-  t.location = "templates/template.json"
-end
+# Commenting out template code until we have actual default content to use
+#template "JSON Template" do |t|
+#  t.filetype = "*.json"
+#  t.location = "templates/template.json"
+#end
